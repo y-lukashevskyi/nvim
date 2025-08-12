9cfb5ef217f5044c7512ca2233a0334bcdfbbecf
Yevhenii Lukashevskyi <aspanio@outlook.com>
2025-08-11 22:39:22 -0500

    fix: shortcuts and add comment

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,7 +33,7 @@ return {
 
 			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>e", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		end,
 	},
--- lua/vim-configs.lua
+++ lua/vim-configs.lua
@@ -1,8 +1,23 @@
 vim.g.maplocalleader = "\\"
 vim.g.mapleader = " "
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
 vim.cmd("set clipboard=unnamedplus")
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
